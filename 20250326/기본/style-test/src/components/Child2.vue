29ca7f021053032778bc8939e098b8129c4566de
glorypnag <dys1128@naver.com>
2025-03-26 15:42:26 +0900

    20250326 기본

---
+++ 20250326/기본/style-test/src/components/Child2.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="child">
+    <h2>Child2</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Child2',
+};
+</script>
+
+<style scopeds>
+.child {
+  background-color: blue;
+  border: 1px solid red;
+  margin: 1.5em;
+  padding: 1em;
+}
+</style>
