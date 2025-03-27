403a2064e152dec539a3b95195940a704b3016b4
glorypnag <dys1128@naver.com>
2025-03-27 14:45:43 +0900

    20250327 기본

---
+++ 20250327/기본/calc-component-test/src/components/Calc.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    X : <input type="text" v-model.number="x" /> <br />
+    Y : <input type="text" v-model.number="y" /><br />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'Calc2',
+  setup() {
+    // 계산 관련 data, method
+    const x = ref(10);
+    const y = ref(20);
+
+    return { x, y };
+  },
+};
+</script>
---
+++ 20250327/기본/calc-component-test/src/components/Calc5.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    x : <input type="text" v-model.number="x" />
+    <br />
+    결과 : {{ result }}
+  </div>
+</template>
+<script>
+import { ref, watch } from 'vue';
+export default {
+  name: 'Calc5',
+  setup() {
+    const x = ref(0); // 0으로 초기화
+    const result = ref(0); // 0으로 초기화
+    watch(x, (current, old) => {
+      console.log(`${current} -> ${old}`);
+      result.value = current * 2;
+    });
+    return { x, result };
+  },
+};
+</script>
